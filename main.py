eda45d08ee471454f41da86c15973004a631c2ac
Your Name <you@example.com>
2022-08-24 15:25:40 +0300

    last

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, redirect
+import sys
+from abc import ABC
+from time import sleep
+
+import schedule
+from flask import Flask
 from flask_admin import Admin, AdminIndexView
+from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
-from flask_sqlalchemy import SQLAlchemy
+from flask_admin.model import BaseModelView
+from flask_admin.model.filters import BaseFilter
 from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore, Security, login_required, current_user
-from celery import Celery
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
@@ -15,22 +22,7 @@ app.config['SECRET_KEY'] = 'asdzxcrfwef'
 
 app.config['SECURITY_PASSWORD_SALT'] = 'salt'
 app.config['SECURITY_PASSWORD_HASH'] = 'bcrypt'
-
-
 db = SQLAlchemy(app)
-
-
-def make_celery(app):
-    celery = Celery(app.import_name)
-    celery.conf.update(app.config["CELERY_CONFIG"])
-    celery.autodiscover_tasks()
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
 
 
 # ограничиваем доступ к админке пользователям без прав
@@ -60,8 +52,8 @@ admin = Admin(app, name='Старая Почта', template_mode='bootstrap3',
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    mass = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    mass = db.Column(db.Float, nullable=False)
 
     country = db.Column(db.String(250), nullable=False)
     city = db.Column(db.String(250), nullable=False)
@@ -69,6 +61,36 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Название {self.title}'
+
+
+class MyBaseFilter(BaseFilter, ABC):
+
+    def __init__(self, column, country, street=None, city=None):
+        super(MyBaseFilter, self).__init__(country, street, city)
+
+        self.column = column
+
+
+class MyEqualFilter(MyBaseFilter):
+    def apply(self, query, value):
+        return query.filter(self.column == value)
+
+    def operation(self):
+        return gettext('equals')
+
+    def validate(self, value):
+        return True
+
+    def clean(self, value):
+        return value
+
+
+class MyDbModel(BaseModelView):
+    def scaffold_filters(self, country):
+        attr = getattr(self.model, Item.country)
+
+        if isinstance(attr, Item.country):
+            return [MyEqualFilter(country, country)]
 
 
 # делаем чтобы был доступ с админки
@@ -153,14 +175,40 @@ def create():
         return render_template('create.html')
 
 
-celery = make_celery(app)
-from logging import info
+from flask import request, render_template, redirect
+from celery import Celery
+
+"""# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:5000/'"""
+
+# Initialize Celery
+celery = Celery()
+# celery.conf.update(app.config)
 
 
-@celery.task()
-def add_together(info):
-    return print(info)
+@celery.task
+def loger_task():
+    logger.addFilter(NoParsingFilter())
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    (logger.info(msg='Current time ' + str(datetime.datetime.now())))
+
+
+import logging
+import datetime
+
+logger = logging.getLogger("logger")
+
+
+class NoParsingFilter(logging.Filter):
+    def filter(self, record):
+        return not record.getMessage().startswith('parsing')
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(60.0, loger_task(), name='add every 60')
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
